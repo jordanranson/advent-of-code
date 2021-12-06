815f10f20c8d71226957d19e8a5f32b3264ee22f
Jordan Ranson <jordan@jordanranson.com>
2021-12-05 20:21:57 -0700

    I'm dumb - day 3.2 bug fix

--- day-3/3_2.lua
+++ day-3/3_2.lua
@@ -1,121 +1,98 @@
+input_oxy = {}
+input_co2 = {}
+
 soln_bit_len = 0
 soln_bit_counts = {}
-soln_most_common_bits = {}
-soln_gamma_rate = '0b0'
-soln_epsilon_rate = '0b0'
-soln_oxygen_generator_rating = '0b0'
-soln_co2_scrubber_rating = '0b0'
-soln_step = 1
-soln_stage = 1
+soln_oxy_rating = '0b0'
+soln_co2_rating = '0b0'
 
-function filter_by_bit (bytes, val, pos) 
+function filter_input (inp, x, val)
   local filtered = {}
 
-  for i = 1, #bytes do
-    if sub(bytes[i],pos,pos) == val then
-      add(filtered, bytes[i])
+  for y = 1, #inp do
+    if sub(inp[y],x,x) == val then
+      add(filtered, inp[y])
     end
   end
 
   return filtered
 end
- 
-function flip_bit (val) 
-  return (val + 1) % 2
-end
 
 function _init ()
   -- prepare input
+  input = input
+  input_oxy = split(input, '\n', false)
+  input_co2 = split(input, '\n', false)
   input = split(input, '\n', false)
   soln_bit_len = #input[1]
 
-  -- prepare tables
-  for i = 1, soln_bit_len do
-    add(soln_bit_counts, {zero = 0, one = 0})
-    add(soln_most_common_bits, 0)
-  end
-end
-
-function _update60 ()
-  if input[soln_step] == nil then return end
-
-  -- count them bits
+  -- filter
+  local filtered
   for x = 1, soln_bit_len do
-    if sub(input[soln_step],x,x) == '0' then
-      soln_bit_counts[x].zero += 1
-    else
-      soln_bit_counts[x].one += 1
+
+    soln_bit_counts = {}
+    for i = 1, soln_bit_len do
+      add(soln_bit_counts, {zero = 0, one = 0})
     end
-  end
 
-  -- find most/least common
-  for x = 1, soln_bit_len do
+    -- count them bits
+    for y = 1, #input_oxy do
+      if sub(input_oxy[y],x,x) == '0' then
+        soln_bit_counts[x].zero += 1
+      else
+        soln_bit_counts[x].one += 1
+      end
+    end
+
+    -- oxygen
     if soln_bit_counts[x].zero > soln_bit_counts[x].one then
-      soln_most_common_bits[x] = 0
+      filtered = filter_input(input_oxy, x, '0')
+    elseif soln_bit_counts[x].zero < soln_bit_counts[x].one then
+      filtered = filter_input(input_oxy, x, '1')
     else
-      soln_most_common_bits[x] = 1
+      filtered = filter_input(input_oxy, x, '1')
+    end
+    if #filtered > 0 then
+      input_oxy = filtered
+    end
+
+    soln_bit_counts = {}
+    for i = 1, soln_bit_len do
+      add(soln_bit_counts, {zero = 0, one = 0})
+    end
+
+    -- count them bits again
+    for y = 1, #input_co2 do
+      if sub(input_co2[y],x,x) == '0' then
+        soln_bit_counts[x].zero += 1
+      else
+        soln_bit_counts[x].one += 1
+      end
+    end
+
+    -- co2
+    if soln_bit_counts[x].zero > soln_bit_counts[x].one then
+      filtered = filter_input(input_co2, x, '1')
+    elseif soln_bit_counts[x].zero < soln_bit_counts[x].one then
+      filtered = filter_input(input_co2, x, '0')
+    else
+      filtered = filter_input(input_co2, x, '0')
+    end
+    if #filtered > 0 then
+      input_co2 = filtered
     end
   end
 
-  -- power rates
-  soln_gamma_rate = '0b'
-  soln_epsilon_rate = '0b'
-
-  for i = 1, soln_bit_len do
-    soln_gamma_rate = soln_gamma_rate .. tostr(soln_most_common_bits[i])
-    soln_epsilon_rate = soln_epsilon_rate .. tostr(flip_bit(soln_most_common_bits[i]))
-  end
-
-  -- life support/co2 scrubber
-  local next_input
-  local filtered = {}
-  
-  -- oxygen generator rating
-  for i = 1, soln_bit_len do
-    if i == 1 or #filtered != 1 then
-      filter_by = '0'
-      if soln_most_common_bits[i] == 1 then filter_by = '1' end
-
-      next_input = filtered
-      if i == 1 then next_input = input end
-
-      filtered = filter_by_bit(next_input, filter_by, i)
-
-      if #filtered == 1 then
-        soln_oxygen_generator_rating = '0b' .. filtered[1]     
-      end 
-    end
-  end
-  
-  -- co2 scrubber rating
-  for i = 1, soln_bit_len do
-    if i == 1 or #filtered != 1 then 
-      filter_by = '1'
-      if flip_bit(soln_most_common_bits[i]) == 0 then filter_by = '0' end
-
-      next_input = filtered
-      if i == 1 then next_input = input end
-
-      filtered = filter_by_bit(next_input, filter_by, i)
-
-      if #filtered == 1 then
-        soln_co2_scrubber_rating = '0b' .. filtered[1]     
-      end 
-    end
-  end
-
-  -- next
-  soln_step += 1
+  -- total
+  soln_oxy_rating = '0b' .. input_oxy[1]
+  soln_co2_rating = '0b' .. input_co2[1]
 end
 
 function _draw ()
 	cls(1)
 
   local color = 6
-  if input[soln_step] == nil then color = 8 end
 
-  print(soln_gamma_rate..' ('..tonum(soln_gamma_rate)..')', 0, 0, color)
-  print(soln_epsilon_rate..' ('..tonum(soln_epsilon_rate)..')', 0, 6, color)
-  print(soln_oxygen_generator_rating..' ('..tonum(soln_oxygen_generator_rating)..')', 0, 12, color)
-  print(soln_co2_scrubber_rating..' ('..tonum(soln_co2_scrubber_rating)..')', 0, 18, color)
+  print(soln_oxy_rating..' ('..tonum(soln_oxy_rating)..')', 0, 0, color)
+  print(soln_co2_rating..' ('..tonum(soln_co2_rating)..')', 0, 6, color)
 end
